b287166e26948570bd21969e62b6f70d327bc140
3JD <3jd@bglab.local>
2024-11-14 08:46:54 -0300

    adicionando tela empresa

--- lib/Models/Empresa.dart
+++ lib/Models/Empresa.dart
@@ -1,16 +1,39 @@
-class Empresa{
-  
- String nome;
- String logo;
- String localizacao;
- String historiaEmpresa;
-
+class Empresa{ 
+  String nome;
+  String logo;
+  String localizacao;
+  String historiaEmpresa;
+  String txtEnvolvimento;
+  List<String> ImgEmpresas;
 
   Empresa({
-
     required this.nome,
     required this.logo,
     required this.localizacao,
     required this.historiaEmpresa,
+    required this.txtEnvolvimento,
+    required this.ImgEmpresas,
   });
+
+  // Método para retornar uma lista de jogos
+  static List<Empresa> getEmpresa() {
+    List<Empresa> empresas = [];
+
+    Empresa empresa1 = Empresa(
+      nome: "Sierra Entertainment",
+      logo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Sierra_Logo%2C_2014.png/220px-Sierra_Logo%2C_2014.png",
+      localizacao: "Oakhurst - California",
+      historiaEmpresa: "A Sierra entertainment ou Sierra On-Line é a empresa fundada por Ken e Roberta Williams 1980 com o nome de On-Line Systems mas que um tempo após o seu primeiro lançamento, o jogo Mystery House eles mudaram sua sede para Oakhurst uma pequena comunidade na localizada na California que é cercada pelo Floresta nacional de Sierra ao redor do parque nacional de Yosemite. Assim a empresa herdaria o nome de Sierra e também manteriam como logo a grande pedra de granito do parque.",
+      txtEnvolvimento: "Roberta Williams tem papel fundamental na empresa, não atoa ela é uma das fundadoras junto de seu marido, durante todos esses anos desenvolvendo jogos na empresa ela sempre inseria algo que transforma aquele produto em uma coisa especial, como Mystery House e King's Quest IV e King's Quest VII ambos protagonizados por mulheres sendo um por uma mãe e uma filha, narrativa essa que na época nem se imaginava qualquer produto desse tipo.",
+      ImgEmpresas: [
+        
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Half_Dome_from_Glacier_Point%2C_Yosemite_NP_-_Diliff.jpg/640px-Half_Dome_from_Glacier_Point%2C_Yosemite_NP_-_Diliff.jpg",
+        "https://images.squarespace-cdn.com/content/v1/5cef493afddef50001d3ae4a/1585253788155-5WDJ4S82H2TF9BVOCVQP/RobertaLorelei.jpg?format=2500w"
+      ]
+    );
+    empresas.add(empresa1);
+
+
+    return empresas;
+  }
 }
--- lib/ViewEmpresa.dart
+++ lib/ViewEmpresa.dart
@@ -1,13 +1,90 @@
+import 'package:app_roberta_williams/Models/Empresa.dart';
 import 'package:flutter/material.dart';
 
 class TelaEmpresa extends StatelessWidget {
   const TelaEmpresa({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    var empresa = Empresa.getEmpresa();
+    int indexJogoAtual = 0;
 
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Empresas Atuantes",
+            style: TextStyle(color: Colors.white),
+          ),
+          leading: IconButton(
+            color: Colors.white,
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          backgroundColor: Colors.black,
+          elevation: 0,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              border:
+                  Border.all(color: Colors.white, width: 1), // Contorno branco
+            ),
+          ),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                      'img/fundo2.jpg',
+                    ),
+                    fit: BoxFit.cover),
+              ),
+              child: Container(color: Colors.black.withOpacity(0.4)),
+            ),
+            Row(
+              children: [
+                 Padding(padding: EdgeInsets.symmetric(
+                 horizontal: 20,
+                 vertical: 50,
+                              )), 
+                Text(empresa[indexJogoAtual].nome,
+               style: TextStyle(color: Colors.white, fontSize: 25)),
+              ]
+            ),
+            Row(
+                  children: [
+                    Padding(padding: EdgeInsets.symmetric(
+                      horizontal: 170,
+                      vertical: 50,
+                    )),
+                   Image.network(empresa[indexJogoAtual].logo,width: 150,)
+                  ],
+                ),
+
+              
+        
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Text(empresa[indexJogoAtual].historiaEmpresa,
+                        style: TextStyle(color: Colors.white, fontSize: 15)),
+                  ),
+                ],
+          
+              ),
+          ],
+      )
+
+        ),
     );
+    
+    
   }
 }
